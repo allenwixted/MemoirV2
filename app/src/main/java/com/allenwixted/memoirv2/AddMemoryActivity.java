b6a264925c8b88587252d82d3e6cc3664dc8bb6a
hydroxate <conorsouness@gmail.com>
2016-04-27 15:43:56 +0100

    Added file writing for most recent location of user

--- app/src/main/java/com/allenwixted/memoirv2/AddMemoryActivity.java
+++ app/src/main/java/com/allenwixted/memoirv2/AddMemoryActivity.java
@@ -1,7 +1,13 @@
 package com.allenwixted.memoirv2;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,11 +20,62 @@ import java.io.FileOutputStream;
 
 public class AddMemoryActivity extends AppCompatActivity {
 
+    double latitude;
+    double longitude;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_memory);
 
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+            Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            double longitude = location.getLongitude();
+            double latitude = location.getLatitude();
+
+        }
+        else
+        {
+            Context context = getApplicationContext();
+            CharSequence text = "GPS Permission Disabled, Please Enable!";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+
+
+        private final LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        }
+
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, locationListener);
 
         Button saveButton = (Button) findViewById(R.id.saveButton);
         if (saveButton != null) {
@@ -41,8 +98,8 @@ public class AddMemoryActivity extends AppCompatActivity {
 
                     writeToFile(getString(R.string.title), String.format("%s\n", titleText));
                     writeToFile(getString(R.string.desc),String.format("%s\n", descText));
-                    writeToFile(getString(R.string.lon),titleText);
-                    writeToFile(getString(R.string.lat),titleText);
+                    writeToFile(getString(R.string.lon),String.format("%f\n", longitude));
+                    writeToFile(getString(R.string.lat),String.format("%f\n", latitude));
 
 
 
