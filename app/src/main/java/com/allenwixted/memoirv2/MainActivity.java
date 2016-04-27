a7048b915482b11074b5cec6bc57fcf211324fd6
Allen Wixted <allenwixted@gmail.com>
2016-04-27 14:48:19 +0100

    Added on click events and intents for main page. - A

--- app/src/main/java/com/allenwixted/memoirv2/MainActivity.java
+++ app/src/main/java/com/allenwixted/memoirv2/MainActivity.java
@@ -1,16 +1,40 @@
 package com.allenwixted.memoirv2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import icepick.State;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button logButton = (Button) findViewById(R.id.mainLogButton);
+        if (logButton != null) {
+            logButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    Intent i = new Intent(getApplicationContext(), AddMemoryActivity.class);
+                    startActivity(i);
+                }
+            });
+        }
+
+        Button viewButton = (Button) findViewById(R.id.mainViewButton);
+        if (viewButton != null) {
+            viewButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    Intent i = new Intent(getApplicationContext(), MapActivity.class);
+                    startActivity(i);
+                }
+            });
+        }
     }
 }
