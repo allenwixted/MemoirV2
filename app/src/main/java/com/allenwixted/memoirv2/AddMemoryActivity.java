23fb193722d888054644c09fc7beec1be4b19773
hydroxate <conorsouness@gmail.com>
2016-04-27 17:10:31 +0100

    Sample Test for Reading Code

--- app/src/main/java/com/allenwixted/memoirv2/AddMemoryActivity.java
+++ app/src/main/java/com/allenwixted/memoirv2/AddMemoryActivity.java
@@ -12,6 +12,7 @@ import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,33 +23,40 @@ import android.widget.Toast;
 import org.w3c.dom.Text;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
-public class AddMemoryActivity extends AppCompatActivity
-{
+public class AddMemoryActivity extends AppCompatActivity {
 
     double latitude;
     double longitude;
+
     @TargetApi(Build.VERSION_CODES.M)
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_memory);
+        final Context context = getApplicationContext();
+
+        //Create a file for each type of info. Titles, Descriptions, longitude and latitude of Saved Memories)
+        File titles = new File(context.getFilesDir(), getString(R.string.title));
+        File desc = new File(context.getFilesDir(), getString(R.string.desc));
+        File lon = new File(context.getFilesDir(), getString(R.string.lon));
+        File lat = new File(context.getFilesDir(), getString(R.string.lat));
 
         //create GPS listener
         LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         //Check permissions of GPS
-        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-        {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
             double longitude = location.getLongitude();
             double latitude = location.getLatitude();
 
         } else //if GPS permission is disabled, notify user to enable it
         {
-            Context context = getApplicationContext();
+
             CharSequence text = "GPS Permission Disabled, Please Enable!";
             int duration = Toast.LENGTH_SHORT;
 
@@ -57,30 +65,25 @@ public class AddMemoryActivity extends AppCompatActivity
         }
 
         //create GPS listener
-        final LocationListener locationListener = new LocationListener()
-        {
+        final LocationListener locationListener = new LocationListener() {
             //If location changes, update variables
-            public void onLocationChanged(Location location)
-            {
+            public void onLocationChanged(Location location) {
                 longitude = location.getLongitude();
                 latitude = location.getLatitude();
             }
 
             @Override
-            public void onStatusChanged(String provider, int status, Bundle extras)
-            {
+            public void onStatusChanged(String provider, int status, Bundle extras) {
 
             }
 
             @Override
-            public void onProviderEnabled(String provider)
-            {
+            public void onProviderEnabled(String provider) {
 
             }
 
             @Override
-            public void onProviderDisabled(String provider)
-            {
+            public void onProviderDisabled(String provider) {
 
             }
         };
@@ -89,15 +92,10 @@ public class AddMemoryActivity extends AppCompatActivity
         lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, locationListener);
 
         Button saveButton = (Button) findViewById(R.id.saveButton);
-        if (saveButton != null)
-        {
-            saveButton.setOnClickListener(new View.OnClickListener()
-            {
+        if (saveButton != null) {
+            saveButton.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onClick(View v)
-                {
-
-                    Context context = getApplicationContext();
+                public void onClick(View v) {
 
                     //Create a file for each type of info. Titles, Descriptions, longitude and latitude of Saved Memories)
                     File titles = new File(context.getFilesDir(), getString(R.string.title));
@@ -109,8 +107,7 @@ public class AddMemoryActivity extends AppCompatActivity
                     EditText title = (EditText) findViewById(R.id.editText);
                     EditText description = (EditText) findViewById(R.id.editText2);
 
-                    if (title != null && description != null)
-                    {
+                    if (title != null && description != null) {
                         String titleText = title.getText().toString();
                         String descText = description.getText().toString();
 
@@ -124,8 +121,14 @@ public class AddMemoryActivity extends AppCompatActivity
                         CharSequence text = "Memory Saved!";
                         int duration = Toast.LENGTH_SHORT;
 
-                        Toast toast = Toast.makeText(context, text, duration);
-                        toast.show();
+                        //Toast toast = Toast.makeText(context, text, duration);
+                        //toast.show();
+
+                        CharSequence text2 = readFile(String.format("%s/%s", context.getFilesDir(), getString(R.string.title)));
+
+                        Toast toast2 = Toast.makeText(context, text2, Toast.LENGTH_LONG);
+                        toast2.show();
+
 
                     }
                 }
@@ -134,48 +137,77 @@ public class AddMemoryActivity extends AppCompatActivity
     }
 
     //Write data to specified file
-    private void writeToFile(String filename, String data )
-    {
+    private void writeToFile(String filename, String data) {
 
         FileOutputStream outputStream;
 
-        try
-        {
+        try {
             outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
             outputStream.write(data.getBytes());
             outputStream.close();
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     //Adds a custom menu to the activity
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
 
     //Detect clicks in the action bar and decide what to do with them
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if(id == R.id.action_map){
+        if (id == R.id.action_map) {
             Intent i = new Intent(getApplicationContext(), MapActivity.class);
             startActivity(i);
             return true;
-        }
-        else if(id == R.id.action_help){
+        } else if (id == R.id.action_help) {
             Intent i = new Intent(getApplicationContext(), helpActivity.class);
             startActivity(i);
             return true;
-        }
-        else{
+        } else {
 
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public static String readFile(String filePath) {
+
+        String result = "";
+        File file = new File(filePath);
+        if (file.exists()) {
+            //byte[] buffer = new byte[(int) new File(filePath).length()];
+            FileInputStream fis = null;
+            try {
+                //f = new BufferedInputStream(new FileInputStream(filePath));
+                //f.read(buffer);
+
+                fis = new FileInputStream(file);
+                char current;
+                while (fis.available() > 0) {
+                    current = (char) fis.read();
+                    result = result + String.valueOf(current);
+
+                }
+
+            } catch (Exception e) {
+                Log.d("MemoryRead", e.toString());
+            } finally {
+                if (fis != null)
+                    try {
+                        fis.close();
+                    } catch (IOException ignored) {
+                    }
+            }
+            //result = new String(buffer);
+        }
+        return result;
     }
 }
