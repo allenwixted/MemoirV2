882bd0f50d7b9165a9f95b2bb9ffb0471bdbaabb
Allen Wixted <allenwixted@gmail.com>
2016-04-28 18:47:17 +0100

    Updated menu choices to all activities.

--- app/src/main/java/com/allenwixted/memoirv2/AddMemoryActivity.java
+++ app/src/main/java/com/allenwixted/memoirv2/AddMemoryActivity.java
@@ -182,20 +182,9 @@ public class AddMemoryActivity extends AppCompatActivity {
     {
         int id = item.getItemId();
 
-        if (id == R.id.action_map)
-        {
-            Intent i = new Intent(getApplicationContext(), MapActivity.class);
-            startActivity(i);
-            return true;
-        } else if (id == R.id.action_help)
-        {
+        if (id == R.id.action_help){
+
             Intent i = new Intent(getApplicationContext(), helpActivity.class);
-            startActivity(i);
-            return true;
-        }
-        else if(id == R.id.action_log)
-        {
-            Intent i = new Intent(getApplicationContext(), AddMemoryActivity.class);
             startActivity(i);
             return true;
         }
